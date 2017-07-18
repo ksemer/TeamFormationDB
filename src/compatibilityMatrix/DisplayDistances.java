26a715a749831834c38d97776545d794d1402c17
Konstantinos Semertzidis <ksemer@cs.uoi.gr>
2017-07-19 00:21:00 +0300

    Create DisplayDistances.java

---
+++ src/compatibilityMatrix/DisplayDistances.java
@@ -0,0 +1,123 @@
+package compatibilityMatrix;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.HashMap;
+
+/**
+ * Display Avg and max distances for SPC and SBP
+ * 
+ * @author ksemer
+ *
+ */
+public class DisplayDistances {
+
+	private static String path_sbp = "wikipedia_sbp_heuristic";
+	private static String path_spc = "wikipedia_spc";
+	private static final int INF = 1000000000;
+	private static final boolean INTERSECTION = false;
+
+	public static void main(String[] args) throws IOException {
+
+		Map<Integer, Map<Integer, Integer>> sbp = new HashMap<>();
+		Map<Integer, Map<Integer, Integer>> spc = new HashMap<>();
+
+		read(spc, path_spc);
+		read(sbp, path_sbp);
+
+		int id1, id2, dist_sbp, dist_spc, max_spc = 0, max_sbp = 0;
+		int count = 0, count_inf = 0, dist_sbp_A = 0, dist_spc_A = 0;
+
+		for (Entry<Integer, Map<Integer, Integer>> entry : spc.entrySet()) {
+			id1 = entry.getKey();
+
+			for (Entry<Integer, Integer> entry1 : entry.getValue().entrySet()) {
+				id2 = entry1.getKey();
+				dist_spc = entry1.getValue();
+
+				Map<Integer, Integer> sbp_ = sbp.get(id1);
+
+				if (sbp_ != null && sbp_.containsKey(id2)) {
+					dist_sbp = sbp_.get(id2);
+				} else {
+					sbp_ = sbp.get(id2);
+
+					if (sbp_ != null && sbp_.containsKey(id1))
+						dist_sbp = sbp_.get(id1);
+					else
+						continue;
+				}
+
+				if (dist_sbp == INF) {
+					count_inf++;
+					continue;
+				}
+
+				if (max_spc < dist_spc)
+					max_spc = dist_spc;
+
+				if (max_sbp < dist_sbp)
+					max_sbp = dist_sbp;
+
+				count++;
+				dist_spc_A += dist_spc;
+				dist_sbp_A += dist_sbp;
+			}
+		}
+
+		System.out.println("Avg dist spc: " + dist_spc_A / count);
+		System.out.println("Avg dist sbp: " + dist_sbp_A / count);
+		System.out.println("Max dist spc: " + max_spc);
+		System.out.println("Max dist sbp: " + max_sbp);
+		System.out.println("#inf sbp: " + count_inf);
+		System.out.println("#pairs: " + count);
+		System.out.println("SBP size: " + sbp.size());
+		System.out.println("SPC size: " + spc.size());
+	}
+
+	private static void read(Map<Integer, Map<Integer, Integer>> map, String path) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(path));
+		String line = null;
+		String[] token;
+		Map<Integer, Integer> m;
+		boolean flag = false;
+
+		System.out.println(path + " is loading");
+
+		if (path.contains("spc"))
+			flag = true;
+
+		int id1, id2, dist, pos;
+
+		while ((line = br.readLine()) != null) {
+			token = line.split("\t");
+			id1 = Integer.parseInt(token[0]);
+			id2 = Integer.parseInt(token[1]);
+
+			if (flag) {
+				dist = Integer.parseInt(token[4]);
+				pos = Integer.parseInt(token[2]);
+
+				if (INTERSECTION) {
+					if (pos == 0)
+						continue;
+				} else if (pos != 0)
+					continue;
+			} else
+				dist = Integer.parseInt(token[2]);
+
+			if ((m = map.get(id1)) == null) {
+				m = new HashMap<>();
+				map.put(id1, m);
+			}
+
+			if (!m.containsKey(id2))
+				m.put(id2, dist);
+		}
+		br.close();
+		System.out.println(path + " is loaded");
+	}
+}
